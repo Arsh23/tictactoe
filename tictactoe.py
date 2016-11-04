2f65e46c9925d01a975a7d025601edd94a342fce
arsh23 <programmer.arsh@gmail.com>
2016-11-04 20:41:53 +0530

    tictactoe finished

--- tictactoe.py
+++ tictactoe.py
@@ -5,6 +5,7 @@ class TicTacToe():
         self.board = ['-' for x in range(9)]
         self.log = []
         self.player = 'A'
+        self.end = False
 
     def display(self):
         for r in [self.board[x: x + 3] for x in range(0, 9, 3)]:
@@ -14,26 +15,59 @@ class TicTacToe():
 
     def move(self, pos):
         if pos < 0 or pos > 8:
-            raise Exception('Invalid position')
+            print 'Invalid position'
         if self.board[pos] != '-':
-            raise Exception('Position already filled')
+            print 'Position already filled'
+        if self.end == True:
+            print 'Game finished'
 
         self.log.append(''.join([self.player, str(pos)]))
         self.board[pos] = 'O' if self.player == 'A' else 'X'
         self.player = 'B' if self.player == 'A' else 'A'
 
         self.display()
-        print self.log
+        if self.check() == 'draw':
+            print "Draw !!"
+            self.end = True
+        elif self.check() != 'no win':
+            print ' {} Won !!'.format(self.check()[0][0])
+            self.end = True
 
     def check(self):
-        pass
-        # from tictactoe import TicTacToe
+        for c in self.board:
+            if c == '-':
+                break
+        else:
+            return 'draw'
+
+        h = [self.board[x: x + 3] for x in range(0, 9, 3)]
+        v = [[self.board[x + y] for x in range(0, 9, 3)] for y in range(3)]
+        d = [[self.board[x] for x in range(y, 9 - y, 4 - y)] for y in [0, 2]]
+
+        all_combos = h + v + d
+        win = False
+        winning_pos = []
+        for i, x in enumerate(all_combos):
+            if x.count(x[0]) == len(x) and x[0] != '-':
+                winning_pos.append(''.join([x[0], str(i)]))
+                win = True
+
+        if win == False:
+            return 'no win'
+        else:
+            return winning_pos
+
+# from tictactoe import TicTacToe
 t = TicTacToe()
 # t.display()
 t.move(4)
 t.move(5)
 t.move(2)
+t.move(6)
 t.move(3)
-t.move(7)
 t.move(1)
-t.display()
+t.move(0)
+t.move(8)
+t.move(7)
+# t.display()
+# t.check()
