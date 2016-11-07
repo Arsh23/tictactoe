e2dff1c8fa05a46f58d19f13ef9251775ae6eb25
arsh23 <programmer.arsh@gmail.com>
2016-11-07 19:46:12 +0530

    fixed errors in TicTacToe

--- tictactoe.py
+++ tictactoe.py
@@ -4,8 +4,9 @@ class TicTacToe():
     def __init__(self):
         self.board = ['-' for x in range(9)]
         self.log = []
+        self.winner = None
         self.player = 'A'
-        self.end = False
+        self.status = None
 
     def display(self):
         for r in [self.board[x: x + 3] for x in range(0, 9, 3)]:
@@ -14,60 +15,36 @@ class TicTacToe():
             print
 
     def move(self, pos):
-        if pos < 0 or pos > 8:
-            print 'Invalid position'
-        if self.board[pos] != '-':
-            print 'Position already filled'
-        if self.end == True:
-            print 'Game finished'
+        # TODO : remove this comdition later
+        if pos < 0 or pos > 8 or self.board[pos] != '-' or self.status != None:
+            print 'Invalid position -', pos
+            return
 
         self.log.append(''.join([self.player, str(pos)]))
         self.board[pos] = 'O' if self.player == 'A' else 'X'
         self.player = 'B' if self.player == 'A' else 'A'
+        result = self.check()
 
-        self.display()
-        if self.check() == 'draw':
+        if result == 'draw':
             print "Draw !!"
-            self.end = True
-        elif self.check() != 'no win':
-            print ' {} Won !!'.format(self.check()[0][0])
-            self.end = True
+            self.status = 'draw'
+        elif result != None:
+            print ' {} Won !!'.format(result[0][0])
+            self.status = 'won'
+            self.winner = result
 
     def check(self):
-        for c in self.board:
-            if c == '-':
-                break
-        else:
-            return 'draw'
-
         h = [self.board[x: x + 3] for x in range(0, 9, 3)]
         v = [[self.board[x + y] for x in range(0, 9, 3)] for y in range(3)]
         d = [[self.board[x] for x in range(y, 9 - y, 4 - y)] for y in [0, 2]]
-
-        all_combos = h + v + d
-        win = False
         winning_pos = []
-        for i, x in enumerate(all_combos):
+
+        for i, x in enumerate(h + v + d):
             if x.count(x[0]) == len(x) and x[0] != '-':
                 winning_pos.append(''.join([x[0], str(i)]))
-                win = True
 
-        if win == False:
-            return 'no win'
-        else:
+        if winning_pos != []:
             return winning_pos
-
-# from tictactoe import TicTacToe
-t = TicTacToe()
-# t.display()
-t.move(4)
-t.move(5)
-t.move(2)
-t.move(6)
-t.move(3)
-t.move(1)
-t.move(0)
-t.move(8)
-t.move(7)
-# t.display()
-# t.check()
+        if '-' not in self.board:
+            return 'draw'
+        return None
